0d5769165e9f1db2232d8c90d650f334bead28ac
hung-phan <colorvisavn@gmail.com>
2023-06-25 18:56:39 -0700

    Update major dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 
 module.exports = {
-  content: ["./pages/**/*.{tsx,ts}", "./share/elements/**/*.{tsx,ts}"],
+  content: ["./app/**/*.{tsx,ts}", "./pages/**/*.{tsx,ts}", "./share/elements/**/*.{tsx,ts}"],
   theme: {
     extend: {},
   },
